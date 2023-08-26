b9e5561f8e6b0c1ec26bde5ee3b5f25b8055a71b
valeartola <mv.artola@alumno.um.edu.ar>
2023-08-26 20:16:04 -0300

    Add

--- Game/scrabble.py
+++ Game/scrabble.py
@@ -3,8 +3,9 @@ from Game.models import BagTiles
 from Game.player import Player
 
 class ScrabbleGame:
-    def __init__(self, player_count):
+    def __init__(self, players_count):
         self.board = Board()
-        self.tile_bag = BagTiles()
-        for _in range(player_count):
-            self.player.append(Player())
+        self.bag_tiles = BagTiles()
+        self.players = []
+        for _ in range(players_count):
+            self.players.append(Player())
