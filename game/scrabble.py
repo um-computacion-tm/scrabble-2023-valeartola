39585615d4332cce7f5fc5006bbbed788998834c
valeartola <mv.artola@alumno.um.edu.ar>
2023-09-06 17:21:03 -0300

    Add

---
+++ game/scrabble.py
@@ -0,0 +1,22 @@
+from game.board import Board
+from game.models import BagTiles
+from game.player import Player
+
+class ScrabbleGame:
+    def __init__(self, players_count: int):
+        self.board = Board()
+        self.bag_tiles = BagTiles()
+        self.players:list[Player] = []
+        for _ in range(players_count):
+            self.players.append(Player(self.bag_tiles))
+        self.current_player = None
+
+    def playing(self):
+        return True
+
+    def next_turn(self):
+        if self.current_player is None:
+            self.current_player = self.players[0]
+        else:
+            index = self.players.index(self.current_player) + 1
+            self.current_player = self.players[1]
