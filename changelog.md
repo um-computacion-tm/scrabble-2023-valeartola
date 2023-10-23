8a0061822d73632569f2c34d7d173adf0ff80635
valeartola <mv.artola@alumno.um.edu.ar>
2023-10-22 22:25:04 -0300

    Add

--- changelog.md
+++ changelog.md
@@ -7,6 +7,11 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.1.8] - 2023-10-22
+
+### Added
+-Add draw tile in player
+
 ## [0.1.7] - 2023-10-21
 
 ### Added
--- game/player.py
+++ game/player.py
@@ -29,36 +29,41 @@ class Player:
     # #         new_tiles = bag.take(needed_tiles)
     # #         self.tiles += new_tiles
 
-def __init__(self):
-        self.name = ""
-        self.tiles = []
-        self.score = 0
+        def __init__(self):
+            self.name = ""
+            self.tiles = []
+            self.score = 0
 
-    def set_name(self,name):
-        self.name = name
-    
-    def get_name(self):
-        return self.name
+        def set_name(self,name):
+            self.name = name
+        
+        def get_name(self):
+            return self.name
 
-    def take_tiles(self, bag:BagTiles, amount):
-        self.tiles.extend(bag.take(amount))
-    
-    def increase_score(self,amount):
-        self.score += amount
+        def take_tiles(self, bag:BagTiles, amount):
+            self.tiles.extend(bag.take(amount))
+        
+        def increase_score(self,amount):
+            self.score += amount
 
-    def get_score(self):
-        return self.score
+        def get_score(self):
+            return self.score
 
-    def refill(self,bag:BagTiles):
-        self.tiles += bag.take(
-            7- len(self.tiles)
-        )
+        def refill(self,bag:BagTiles):
+            self.tiles += bag.take(
+                7- len(self.tiles)
+            )
 
-    def has_letters(self, tiles=[]):
-        letras_jugador = [tile.letter for tile in self.tiles]
-        letras_palabra = [tile.letter for tile in tiles]
-        letras_necesarias = Counter(letras_palabra)
-        for letra, cantidad in letras_necesarias.items():
-            if letras_jugador.count(letra) < cantidad:
-                return False
-        return True
+        def has_letters(self, tiles=[]):
+            letras_jugador = [tile.letter for tile in self.tiles]
+            letras_palabra = [tile.letter for tile in tiles]
+            letras_necesarias = Counter(letras_palabra)
+            for letra, cantidad in letras_necesarias.items():
+                if letras_jugador.count(letra) < cantidad:
+                    return False
+            return True
+        
+        def draw_tiles(self, bag, num_tiles):
+            if num_tiles <= len(bag.tiles):
+                self.tiles.extend(bag.tiles[:num_tiles])
+                del bag.tiles[:num_tiles]
