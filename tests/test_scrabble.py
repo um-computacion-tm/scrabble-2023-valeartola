53bfc081f8a82bc57f499c9a2dc4732066b9d179
valeartola <mv.artola@alumno.um.edu.ar>
2023-10-24 23:08:22 -0300

    Add

--- tests/test_scrabble.py
+++ tests/test_scrabble.py
@@ -2,18 +2,22 @@ import unittest
 from game.scrabble import ScrabbleGame
 
 class TestScrabbleGame(unittest.TestCase):
-    def test_init(self):
-        scrabble_game = ScrabbleGame(players_count=3)
+    def test_init_(self):
+        scrabble_game = ScrabbleGame(players_count = 3)
         self.assertIsNotNone(scrabble_game.board)
-        self.assertEqual(
-            len(scrabble_game.players),
-            3,
-        )
+        self.assertEqual(len(scrabble_game.players),3)
         self.assertIsNotNone(scrabble_game.bag_tiles)
+        # scrabble_game = ScrabbleGame(players_count=3)
+        # self.assertIsNotNone(scrabble_game.board)
+        # self.assertEqual(
+        #     len(scrabble_game.players),
+        #     3,
+        # )
+        # self.assertIsNotNone(scrabble_game.bag_tiles)
     
     def test_next_turn_when_game_is_starting(self):
         # Validar que al comienzo, el turno es del jugador 0        
-        scrabble_game = ScrabbleGame(players_count=3)
+        scrabble_game = ScrabbleGame(players_count = 3)
         scrabble_game.next_turn()
 
         self.assertEqual(scrabble_game.current_player, scrabble_game.players[0])
