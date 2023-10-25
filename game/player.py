4bff07c20e189dff1c1aa81c49fbdda9061979b1
valeartola <mv.artola@alumno.um.edu.ar>
2023-10-24 23:07:58 -0300

    Add

--- game/player.py
+++ game/player.py
@@ -1,33 +1,7 @@
 from game.models import BagTiles
+from collections import Counter
 
 class Player:
-    # def __init__(self, id:int):
-    #     self.id = id
-    #     bag = BagTiles()
-    #     self.tiles = bag.take(7)
-    #     self.score = 0
-
-
-    # def set_name(self, name = None):
-    #     self.name = name
-
-    # def get_name(self):
-    #     return self.name
-    
-    
-    # def has_letters(self, tiles):
-    #     player_bag = self.tiles
-    #     for tile in tiles:
-    #         if tile.letter not in player_bag:
-    #             return False
-    #         else:
-    #             return True
-        
-    # # def refill(self, bag):
-    # #     needed_tiles = 7 - len(self.tiles)
-    # #     if needed_tiles > 0:
-    # #         new_tiles = bag.take(needed_tiles)
-    # #         self.tiles += new_tiles
 
         def __init__(self):
             self.name = ""
@@ -39,16 +13,19 @@ class Player:
         
         def get_name(self):
             return self.name
-
-        def take_tiles(self, bag:BagTiles, amount):
-            self.tiles.extend(bag.take(amount))
         
         def increase_score(self,amount):
             self.score += amount
 
         def get_score(self):
             return self.score
+        
+        def show_tiles(self):
+            return self.tiles        
 
+        def take_tiles(self, bag:BagTiles, amount):
+            self.tiles.extend(bag.take(amount))
+        
         def refill(self,bag:BagTiles):
             self.tiles += bag.take(
                 7- len(self.tiles)
